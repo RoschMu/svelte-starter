0f6cc660fdd5294fc02dd71f1aebbff8d7b6b4e7
RoschMu <47240674+RoschMu@users.noreply.github.com>
2021-05-24 20:19:19 +0200

    first commit

---
+++ src/pages/Persons.svelte
@@ -0,0 +1,38 @@
+<script>
+  import axios from "axios";
+  import { onMount } from "svelte";
+
+  let persons = [];
+
+  onMount(() => {
+    getPerson();
+  });
+
+  function getPerson() {
+    axios.get("http://localhost:8080/infections/persons").then((response) => {
+      persons = response.data;
+    });
+  }
+</script>
+
+<h1>This is the list of all persons</h1>
+
+<!-- svelte-ignore component-name-lowercase -->
+<table class="table">
+  <thead>
+    <tr>
+      <th>ID</th>
+      <th>Name</th>
+      <th>Birthdate</th>
+    </tr>
+  </thead>
+  <tbody>
+    {#each persons as person}
+      <tr>
+        <td>{person.id}</td>
+        <td>{person.name}</td>
+        <td>{person.birthdate}</td>
+      </tr>
+    {/each}
+  </tbody>
+</table>
